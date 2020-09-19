62e4f4f077dda4946abc2669ac982d576500974a
Henrique Mariano <henrique.mariano@hotmail.com>
2020-09-19 10:13:53 -0300

    Add Bootstrap e finaliza tela de carregamento

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <div class="contagem">
       <div class="formulario_contagem">
-        <h2>Contagens de passagem pelo refeitorio</h2>
-        <form @submit.prevent="receberContadores()">
+        <h2 class="mb-3">Contagens de passagem pelo refeitorio</h2>
+        <form @submit.prevent="receberContadores()" @submit="loading = true">
           <label for="data_contagem">Data</label>
-          <input id="data_contagem" type="date" name="data_contagem" v-model="contadores.data"/>
+          <input id="data_contagem" type="date" name="data_contagem" v-model="contadores.data" required/>
 
           <label for="hora_inicial_contagem">Hora Inicial</label>
-          <input id="hora_inicial_contagem" type="time" name="hora_inicial_contagem" v-model="contadores.horaInicial"/>
+          <input id="hora_inicial_contagem" type="time" name="hora_inicial_contagem" v-model="contadores.horaInicial" required/>
 
           <label for="hora-final-contagem">Hora Final</label>
-          <input id="hora-final-contagem" type="time" name="hora-fina-contagem" v-model="contadores.horaFinal"/>
+          <input id="hora-final-contagem" type="time" name="hora-fina-contagem" v-model="contadores.horaFinal" required/>
 
-          <input type="submit" value="Consulta">
+          <div class="row d-flex justify-content-center">
+            <input type="submit" value="Consulta" class="btn btn-block btn-info col-2 mt-2">
+          </div>
         </form>
       </div>
-      <div class="tabela-contagem">
-        <table>
-          <thead>
-          <tr>
-            <th>Dia</th>
-            <th>Hora Inicial</th>
-            <th>Hora Final</th>
-            <th>Refeitorio 1</th>
-            <th>Refeitorio 2</th>
-          </tr>
-          </thead>
-          <tbody>
+      <div class="row mt-3">
+        <div class="col-2 "></div>
+        <div class="col-8">
+          <table class="table table-borderless">
+            <thead>
+            <tr>
+              <th>Dia</th>
+              <th>Hora Inicial</th>
+              <th>Hora Final</th>
+              <th>Refeitorio 1</th>
+              <th>Refeitorio 2</th>
+            </tr>
+            </thead>
+            <tbody>
             <tr>
               <td>{{ dia }}</td>
               <td>{{ horaInicial }}</td>
@@ -35,32 +39,46 @@
               <td>{{ refeitorio1 }}</td>
               <td>{{ refeitorio2 }}</td>
             </tr>
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
+        <div class="col-2 "></div>
       </div>
+
     </div>
-    <div class="relatorios">
-      <h2>Gerar relatorio das catracas no Excel</h2>
+    <hr>
+    <div class="relatorios container">
+      <h2 class="mb-3">Gerar relatorio das catracas no Excel</h2>
       <div class="formulario-relatorio">
         <form @submit.prevent="gerarRelatorio()" @submit="loading = true">
           <label for="data_inicial_relatorio">Data Inicial</label>
-          <input type="date" name="" id="data_inicial_relatorio" v-model="relatorio.dataInicial">
+          <input type="date" name="" id="data_inicial_relatorio" v-model="relatorio.dataInicial" required>
           <label for="data_final_relatorio">Data Final</label>
-          <input type="date" name="" id="data_final_relatorio" v-model="relatorio.dataFinal">
+          <input type="date" name="" id="data_final_relatorio" v-model="relatorio.dataFinal" required>
           <label for="hora_inicial_relatorio">Hora Inicial</label>
-          <input type="time" name="" id="hora_inicial_relatorio" v-model="relatorio.horaInicial">
+          <input type="time" name="" id="hora_inicial_relatorio" v-model="relatorio.horaInicial" required>
           <label for="final_inicial_relatorio">Hora Final</label>
-          <input type="time" name="" id="final_inicial_relatorio" v-model="relatorio.horaFinal">
-          <input type="submit" value="Gerar">
+          <input type="time" name="" id="final_inicial_relatorio" v-model="relatorio.horaFinal" required>
+          <div class="row d-flex justify-content-center">
+            <input type="submit" value="Gerar" class="btn btn-block btn-info col-2 mt-2">
+          </div>
         </form>
-          <div class="carregando-animacao" v-show="loading">
+      </div>
+    </div>
+    <div class="carregamento-geral container-fluid" v-show="loading">
+      <div class="carregando-animacao" >
+        <div class="row d-flex justify-content-center">
+          <div class="carregando-animacao">
             <div class="hollow-dots-spinner" :style="spinnerStyle">
               <div class="dot"></div>
               <div class="dot"></div>
               <div class="dot"></div>
-              <div class="texto-carregando"><p><b>Carregando...aguarde</b></p></div>
             </div>
           </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+          <div class="texto-carregando"><p><b>Carregando...aguarde</b></p></div>
+        </div>
       </div>
     </div>
   </div>
@@ -102,51 +120,57 @@ export default {
         })
     },
 
-    receberContadores(){
+    receberContadores() {
       this.$http.post('http://localhost:3000/contagem/refeitorios', this.contadores)
-      .then(res => res.json())
-      .then(result => {this.refeitorio1= result.refeitorio1, this.refeitorio2= result.refeitorio2,
-        this.horaInicial = result.horaInicial, this.horaFinal = result.horaFinal, this.dia = result.dia}, err => console.log(err))
-      }
-
+        .then(res => res.json())
+        .then(result => {
+          this.refeitorio1 = result.refeitorio1, this.refeitorio2 = result.refeitorio2,
+            this.horaInicial = result.horaInicial, this.horaFinal = result.horaFinal, this.dia = result.dia, this.loading = false
+        }, err => console.log(err))
     }
+
+  }
 }
 </script>
 
 <style>
+body {
+  background: #c8c8c8;
+  background-attachment: fixed;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 65px;
 
 }
 
-.tabela-contagem {
-  margin: 0 auto;
-  text-align: center;
-  background: red;
+.carregamento-geral {
+  position: absolute;
+  left:0;
+  top:0;
   width: 100%;
-  align: center;
-}
-
-.relatorios{
-
-
-}
-.texto-carregando{
-  color:#00102f;
-  font-size:25px;
-  margin-left: -40px;
+  height: 100%;
+  background: rgba(200,200,200,0.8);
+  margin: 0;
 }
 .carregando-animacao{
-  width: 260px;
-  position:relative;
-  margin-left: -130px;
-  left: 50%;
+  height: 100px;
+  position: relative;
+  margin-top: -50px;
+  top:50%
 }
+
+.texto-carregando {
+  color: #00102f;
+  font-size: 25px;
+}
+
+
 
 
 .hollow-dots-spinner, .hollow-dots-spinner * {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { BootstrapVue } from 'bootstrap-vue'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   el: '#app',
